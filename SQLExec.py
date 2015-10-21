d96a02745936d0f0880735287d6b68edc3a72879
Megan <megan.lkm@gmail.com>
2015-10-21 00:46:51 -0500

    add sqlite support; need to refactor

--- SQLExec.py
+++ SQLExec.py
@@ -48,19 +48,25 @@ class Connection:
 
         tables = []
         for result in command.run().splitlines():
-            try:
-                tables.append(result.split('|')[1].strip())
-            except IndexError:
-                pass
-
+            if result and '|' in result:
+                try:
+                    tables.append(result.split('|')[1].strip())
+                except IndexError:
+                    pass
+            else:
+                tables.append(result)
         os.unlink(self.tmp.name)
         return tables
 
     def descTable(self, tableName):
         self.query = self.settings['queries']['desc table']['query'] % tableName
         command = self._getCommand(self.settings['queries']['desc table']['options'], self.query)
-        command.show()
+        tabledesc = command.run()
 
+        if self.db_type == 'sqlite':
+            tabledesc = SQLite(table_name=tableName, result=tabledesc).get_table_desc()
+
+        command.show(tabledesc)
         os.unlink(self.tmp.name)
 
     def showTableRecords(self, tableName):
@@ -118,8 +124,9 @@ class Command:
 
         return results.decode('utf-8', 'replace').replace('\r', '')
 
-    def show(self):
-        results = self.run()
+    def show(self, results=None):
+        if results is None:
+            results = self.run()
         if results:
             self._display("SQLExec Results", results)
 
@@ -141,28 +148,118 @@ class Selection:
 
 class Options:
     def __init__(self, name):
-        self.name     = name
-        connections   = sqlexec_settings.get('connections')
-        self.type     = connections[self.name]['type']
-        self.host     = connections[self.name]['host']
-        self.port     = connections[self.name]['port']
+        self.name = name
+        connections = sqlexec_settings.get('connections')
+        self.type = connections[self.name]['type']
+        self.host = connections[self.name]['host']
+        self.port = connections[self.name]['port']
         self.username = connections[self.name]['username']
         self.password = connections[self.name]['password']
         self.database = connections[self.name]['database']
         if 'service' in connections[self.name]:
-            self.service  = connections[self.name]['service']
+            self.service = connections[self.name]['service']
 
     def __str__(self):
         return self.name
 
     @staticmethod
     def list():
-        names = []
-        connections = sqlexec_settings.get('connections')
-        for connection in connections:
-            names.append(connection)
-        names.sort()
-        return names
+        return sorted([conn for conn in sqlexec_settings.get('connections')])
+
+
+class SQLite(object):
+
+    result_template = Template('Table: $table\n$data\n\nRaw:\n$raw')
+
+    def __init__(self, table_name=None, result=None, table_meta=None):
+        self.table_name = table_name
+        self.result = result
+        self.table_meta = table_meta
+
+    def _prepare_desc_result(self):
+        self.table_name = self.table_name.strip()
+        self.raw_data = self.result
+        self.table_meta = self.result.split('\n')
+        self.table_desc = self.table_meta.pop(0)
+
+    def _prepare_desc_indexes(self):
+        self.indexes = list()
+        for index in self.table_meta:
+            i = self.parse_index(index)
+            if i:
+                self.indexes.append(i)
+
+    def get_cell_len(self, data, key):
+        """ data is a list of dicts and key is a string """
+        vals = [str(i[key]) for i in data]
+        vals.append(key)
+        return len(max([i for i in vals], key=len))
+
+    def parse_results(self, result_list):
+        result_list = [i for i in result_list if i != '']
+
+    def get_table_desc(self):
+        self._prepare_desc_result()
+        self._prepare_desc_indexes()
+        self.data = self.format_table_desc()
+        return self.result_template.substitute(table=self.table_name, data=self.data, raw=self.raw_data)
+
+    def format_table_desc(self):
+        self.table_desc = self.table_desc.strip()
+        self.table_desc = re.sub(r'CREATE TABLE "{}" \('.format(self.table_name), '', self.table_desc)
+        self.table_desc = re.sub(r'\)\;$', '', self.table_desc)
+        self.table_desc = self.table_desc.replace('"', '')
+
+        rows = list()
+        uniq = list()
+        m = re.match(r".*UNIQUE\s\((.*)\)", self.table_desc)
+        if m:
+            try:
+                uniq = m.group(1)
+                uniq = uniq.split(',')
+                self.table_desc = re.sub(r"\, UNIQUE\s\(.*\)", '', self.table_desc)
+            except:
+                uniq = list()
+
+        parselist = self.table_desc.split(',')
+        parselist = [p.strip() for p in parselist]
+
+        for row in parselist:
+            fields = dict()
+            row = row.split(' ', 2)
+
+            fields['NAME'] = row.pop(0)
+            fields['TYPE'] = row.pop(0)
+            fields['INDEX'] = '' if fields['NAME'] not in self.indexes else 'X'
+            fields['UNIQUE'] = '' if fields['NAME'] not in uniq else 'X'
+            fields['ATTRIBUTES'] = ' '.join(row)
+            rows.append(fields)
+
+        name_len = self.get_cell_len(rows, 'NAME')
+        type_len = self.get_cell_len(rows, 'TYPE')
+        index_len = self.get_cell_len(rows, 'INDEX')
+        unique_len = self.get_cell_len(rows, 'UNIQUE')
+        attr_len = self.get_cell_len(rows, 'ATTRIBUTES')
+
+        header_formatter = '| {:^%s} | {:^%s} | {:^%s} | {:^%s} | {:^%s} |' % (name_len, type_len, index_len, unique_len, attr_len)
+        data_formatter = '| {:<%s} | {:<%s} | {:^%s} | {:^%s} | {:<%s} |' % (name_len, type_len, index_len, unique_len, attr_len)
+        the_table_tr_border = '+-{}-+-{}-+-{}-+-{}-+-{}-+'.format('-'*name_len, '-'*type_len, '-'*index_len, '-'*unique_len, '-'*attr_len)
+
+        the_table = '{}\n{}\n{}\n'.format(the_table_tr_border,
+                                          header_formatter.format('NAME', 'TYPE', 'INDEX', 'UNIQUE', 'ATTRIBUTES'),
+                                          the_table_tr_border)
+        for row in rows:
+            the_table = '{}{}\n'.format(the_table,
+                                        data_formatter.format(row['NAME'], row['TYPE'], row['INDEX'], row['UNIQUE'], row['ATTRIBUTES']))
+        return '{}{}'.format(the_table, the_table_tr_border)
+
+    def parse_index(self, table_meta):
+        m = re.match(r"^CREATE\sINDEX \"\w+\"\sON\s\"\w+\"\s\(\"(\w+)\"\);", table_meta)
+        try:
+            return m.group(1)
+        except:
+            pass
+        return None
 
 
 def sqlChangeConnection(index):
@@ -177,8 +274,7 @@ def showTableRecords(index):
     global connection
     if index > -1:
         if connection is None:
-            sublime.error_message('No active connection')
-            return
+            return sublime.error_message('No active connection')
         tables = connection.desc()
         connection.showTableRecords(tables[index])
 
@@ -187,8 +283,7 @@ def descTable(index):
     global connection
     if index > -1:
         if connection is None:
-            sublime.error_message('No active connection')
-            return
+            return sublime.error_message('No active connection')
         tables = connection.desc()
         connection.descTable(tables[index])
 
@@ -218,8 +313,7 @@ class sqlDesc(sublime_plugin.WindowCommand):
     def run(self):
         global connection
         if connection is None:
-            sublime.error_message('No active connection')
-            return
+            return sublime.error_message('No active connection')
         tables = connection.desc()
         sublime.active_window().show_quick_panel(tables, descTable)
 
@@ -228,8 +322,7 @@ class sqlShowRecords(sublime_plugin.WindowCommand):
     def run(self):
         global connection
         if connection is None:
-            sublime.error_message('No active connection')
-            return
+            return sublime.error_message('No active connection')
         tables = connection.desc()
         sublime.active_window().show_quick_panel(tables, showTableRecords)
 
@@ -239,8 +332,7 @@ class sqlQuery(sublime_plugin.WindowCommand):
         global connection
         global history
         if connection is None:
-            sublime.error_message('No active connection')
-            return
+            return sublime.error_message('No active connection')
         sublime.active_window().show_input_panel('Enter query', history[-1], executeQuery, None, None)
 
 
@@ -248,8 +340,7 @@ class sqlExecute(sublime_plugin.WindowCommand):
     def run(self):
         global connection
         if connection is None:
-            sublime.error_message('No active connection')
-            return
+            return sublime.error_message('No active connection')
         selection = Selection(self.window.active_view())
         connection.execute(selection.getQueries())
 
